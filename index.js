a4d506b5c5fff880e2fc951451795b8e0c2ba91b
AlexMontesdeOca <la.montesdeoca.p@gmail.com>
2020-08-09 13:36:06 -0500

    cambio en los headers del cors

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const app = express();
 
     //Headers del cors
     app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "https://mern-tast-react.netlify.app/"); // update to match the domain you will make the request from
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, X-Request-With");
         res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
         next();
       });
